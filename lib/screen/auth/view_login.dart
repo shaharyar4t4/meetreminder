36cdba92dfa6ed85137d74af9d1ed103b1728937
Shaharyar <shaharyar4t4@gmail.com>
2025-06-05 18:14:24 +0500

    updated

--- lib/screen/auth/view_login.dart
+++ lib/screen/auth/view_login.dart
@@ -1,18 +1,220 @@
 import 'package:flutter/material.dart';
+import 'package:meetreminder/screen/services/auth_services.dart';
 
-class ViewLogin extends StatefulWidget {
-  const ViewLogin({super.key});
+class VeiwLogin extends StatefulWidget {
+  const VeiwLogin({super.key});
 
   @override
-  State<ViewLogin> createState() => _ViewLoginState();
+  State<VeiwLogin> createState() => _VeiwLoginState();
 }
 
-class _ViewLoginState extends State<ViewLogin> {
+class _VeiwLoginState extends State<VeiwLogin> {
+  TextEditingController email = TextEditingController();
+  TextEditingController pass = TextEditingController();
+  AuthServices authServices = AuthServices();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Login")),
-      body: Text("hello"),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 10),
+              Center(
+                child: Image.asset(
+                  'assets/image/Group.png',
+                  width: 170,
+                  height: 170,
+                ),
+              ),
+              SizedBox(height: 13),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 19),
+                child: Column(
+                  spacing: 13,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Loging",
+                      style: TextStyle(
+                        fontFamily: 'gilroy',
+                        color: Colors.black,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "Enter your emails and password",
+                      style: TextStyle(
+                        fontFamily: 'gilroy',
+                        color: Colors.grey,
+                        fontSize: 13,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 30),
+              Center(
+                child: Container(
+                  width: 320,
+                  child: TextField(
+                    controller: email,
+                    decoration: InputDecoration(
+                      label: Text(
+                        "Email",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontFamily: 'gilroy',
+                        ),
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.grey.shade200,
+                        ), // Default color
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.grey.shade200,
+                          width: 1.0,
+                        ), // Change color when focused
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              Center(
+                child: Container(
+                  width: 325,
+                  child: TextField(
+                    obscureText: true,
+                    controller: pass,
+                    decoration: InputDecoration(
+                      label: Text(
+                        "Password",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontFamily: 'gilroy',
+                        ),
+                      ),
+                      suffixIcon: Icon(
+                        Icons.visibility_off,
+                        color: Colors.grey,
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.grey.shade200,
+                        ), // Default color
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.grey.shade200,
+                          width: 1.0,
+                        ), // Change color when focused
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              Padding(
+                padding: const EdgeInsets.only(left: 230),
+                child: Text(
+                  "Forget Password?",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 13,
+                    fontFamily: 'gilroy',
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: ElevatedButton(
+                  onPressed: () async {
+                    String? result = await authServices.login(
+                      email.text,
+                      pass.text,
+                    );
+                    if (result == "success") {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          backgroundColor: Colors.green,
+                          content: Text(
+                            "Your are login now...",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      );
+                      // Navigate to Home Page if login is successful
+                      // Navigator.pushReplacement(
+                      //   context,
+                      //   MaterialPageRoute(builder: (context) => ViewHome()),
+                      // );
+                    } else {
+                      // Show error message if login fails
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          backgroundColor: Colors.red,
+                          content: Text(
+                            result ?? "Login failed",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.tealAccent,
+                    minimumSize: Size(300, 65),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                  ),
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                      fontFamily: 'Gilroy',
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an account? ",
+                    style: TextStyle(fontFamily: 'gilroy', color: Colors.black),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      // Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(builder: (context) => ViewSignup()),
+                      // );
+                    },
+                    child: Text(
+                      "Sign up",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'gilroy',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
